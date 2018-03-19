3bfab4c3615e407fe7ffcc134dad0ca5c7b8ee28
Alex <mersereau.alex@gmail.com>
2018-03-19 16:23:24 -0400

    bullet shooter
    I guess they could be renamed later or something lol.

---
+++ Bullet_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Script : MonoBehaviour {
+
+	public float speed = 1;
+	public Vector3 direction;
+	public bool useInternalForward;
+
+	/* 
+	void Start (Vector3 inDirection, float inSpeed = 1, bool inUseInternalForward = false) {
+	 	direction = inDirection;
+	 	speed = inSpeed;
+	 	useInternalForward = inUseInternalForward;
+	} */
+
+	// Start() can't take parameters lol
+
+	//public void Fire(Vector3 inDirection, float inSpeed = 1, bool inUseInternalForward = false) {
+	public void Fire(Vector3 inDirection, float inSpeed = 1, bool inUseInternalForward = false) {
+		direction = inDirection;
+		speed = inSpeed;
+		useInternalForward = inUseInternalForward;
+	}
+
+	void Update () {
+		if (useInternalForward)
+			direction = transform.forward;
+		transform.Translate(direction * speed); 
+	}
+}
---
+++ Shoot_Bullets.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Bullets : MonoBehaviour {
+
+	public Rigidbody2D bullet;
+	private float timer;		//internal; counts how long since last bullet fire
+	public int frequency;		//seconds between firing
+
+	void Start(){
+		timer = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > frequency) {
+			shootBullet ();
+			timer = 0;
+		}
+	}
+
+	void shootBullet(){
+		Rigidbody2D firedBullet;
+		firedBullet = Instantiate (bullet, transform.position, transform.rotation) as Rigidbody2D;
+		firedBullet.velocity = transform.TransformDirection (Vector3.forward * 2);
+	}
+}
